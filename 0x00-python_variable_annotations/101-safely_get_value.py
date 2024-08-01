6f3523eb33143ee010044a1e8f0f71c92149cb21
KennyD4Christ <kennyd4christ@gmail.com>
2024-08-01 19:20:47 +0100

    101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python3
 """
-This module contains a function that safely retrieves a value
-from a dictionary.
+Module to Safely get a value from a dictionary by key, returning
+a default value if the key is not present
 """
+from typing import TypeVar, Mapping, Any, Union
 
-from typing import Mapping, Any, Union, TypeVar
-
-# Define a type variable that can be any type
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[Any, T], key: Any,
-                     default: Union[T, None] = None) -> Union[T, None]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """
-    Safely get a value from a dictionary.
+    Safely get a value from a dictionary by key, returning a default
+    value if the key is not present.
 
-    Args:
-        dct (Mapping[Any, T]): A dictionary where the keys are of any type and
-        values are of type T.
-        key (Any): The key to look for in the dictionary.
-        default (Union[T, None], optional): The default value to return if
-        the key is not found. Defaults to None.
-
-    Returns:
-        Union[T, None]: The value associated with the key if it exists,
-        otherwise the default value or None.
+    :param dct: The dictionary from which to get the value.
+    :param key: The key whose value should be retrieved.
+    :param default: The value to return if the key is not present
+    in the dictionary.
+    :return: The value associated with the key if it is present, otherwise
+    the default value.
     """
     if key in dct:
         return dct[key]
